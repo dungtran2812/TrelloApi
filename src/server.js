79ba8c2281c7ecfb416ec8f1696e686e3e812508
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-05-18 23:11:11 +0700

    add valdation and error handling

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from './middlewares/errorhandlingMiddleware'
 
 
 const START_SERVER = () => {
@@ -12,6 +13,8 @@ const START_SERVER = () => {
   //Enable req.body json data
   app.use(express.json())
   app.use('/v1', APIs_V1)
+  //middleware tập trung xử lý lỗi
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)

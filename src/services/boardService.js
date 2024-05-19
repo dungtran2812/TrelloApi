c08d9672614938e2253f5e67e5a4c31dc9492eca
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-05-19 15:16:33 +0700

    done api create board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-useless-catch */
+import { boardModel } from '~/models/boardModel'
 import { slugify } from '~/utils/formatters'
 const createNew = async(reqBody) => {
   try {
@@ -6,7 +7,11 @@ const createNew = async(reqBody) => {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    return newBoard
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log('createdBoard', createdBoard)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log('createdBoard.insertedId', createdBoard.insertedId)
+    return getNewBoard
   } catch (error) {
     throw error
   }
